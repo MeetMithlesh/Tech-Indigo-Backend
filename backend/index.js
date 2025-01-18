6431f36b0afceabcfe355481a1ed12bc800f6ef2
Meet_Mithles <mithleshmithlesh555@gmail.com>
2025-01-19 01:05:40 +0530

    Co-authored-by: harshita12.pishwe@gmail.com <harshita12.pishwe@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const db = require("./models/db.js");
 const path = require("path");
 const app = express();
@@ -10,10 +9,11 @@ const userRouter = require("./routes/register.js");
 const paymentRouter = require("./routes/payment.js");
 const quizRouter = require("./routes/quiz.js");
 
-app.use(cors());
+app.use(cors(
+    {origin:'https://tech-indigo-event.vercel.app'}
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../tech-indigo")));
 app.use(express.json());
 
@@ -21,15 +21,17 @@ app.use("/api", userRouter);
 // app.use("/api", paymentRouter);
 app.use('/api', quizRouter);
 
-app.get("/api", (req, res) => {
-    res.sendFile(path.join(__dirname, "../tech-indigo/index.html"));
-    });
+// app.get("/api", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../tech-indigo/index.html"));
+//     });
 app.get("/api/quiz", (req, res) => {
-    res.sendFile(path.join(__dirname, "../tech-indigo/quiz.html"));
+    // res.sendFile(path.join(__dirname, "../tech-indigo/quiz.html"));
+    res.redirect('https://tech-indigo-event.vercel.app/quiz.html')
     });
 
 app.get("/api/developers", (req, res) => {
-    res.sendFile(path.join(__dirname, "../tech-indigo/developers.html"));
+    // res.sendFile(path.join(__dirname, "../tech-indigo/developers.html"));
+    res.redirect('https://tech-indigo-event.vercel.app/developers.html')
     });
 
 
